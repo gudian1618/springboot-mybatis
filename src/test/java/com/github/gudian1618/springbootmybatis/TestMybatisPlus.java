3e40ab7ad018e39d67de9e14d2083cb4622b6148
gudian1618 <1761844211@qq.com>
2020-03-02 19:14:14 +0800

    new

--- src/test/java/com/github/gudian1618/springbootmybatis/TestMybatisPlus.java
+++ src/test/java/com/github/gudian1618/springbootmybatis/TestMybatisPlus.java
@@ -1,5 +1,7 @@
 package com.github.gudian1618.springbootmybatis;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.github.gudian1618.springbootmybatis.mapper.UserMapper;
 import com.github.gudian1618.springbootmybatis.pojo.User;
 import org.junit.Test;
@@ -7,6 +9,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * @author gudian1618
@@ -27,5 +31,47 @@ public class TestMybatisPlus {
         user.setName("mybatisPlus").setAge(18).setSex("男");
         int rows = userMapper.insert(user);
         System.out.println(rows);
+    }
+
+    // 查询用户列表信息
+    // 将对象中部位null的属性当做where条件
+    // 条件:age=18
+    // 等于eq 大于gt 小于lt 大于等于ge 小于等于le
+    @Test
+    public void testFind() {
+        // 1.方式一:使用对象封装
+//        User user = new User();
+//        user.setAge(18);
+
+        // 2.直接使用字段赋值
+//        QueryWrapper<User> queryWrapper = new QueryWrapper<>(user);
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        // 测试等于 eq
+//        queryWrapper.eq("age", 18);
+        // 测试大于 gt
+        queryWrapper.ge("age", 18);
+        List<User> userList = userMapper.selectList(queryWrapper);
+        System.out.println(userList);
+    }
+
+    /**
+     * 3.数据更新
+     * entity:需要修改数据的值
+     * updateWrapper 条件构造器 充当where条件
+     */
+    @Test
+    public void testUpdate() {
+        User user = new User();
+        user.setAge(19);
+        UpdateWrapper<User> UpdateWrapper = new UpdateWrapper<>();
+        userMapper.update(user, UpdateWrapper);
+    }
+
+    // 删除name字段为null的数据
+    @Test
+    public void deleteUser() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.isNull("name");
+        userMapper.delete(queryWrapper);
     }
 }
